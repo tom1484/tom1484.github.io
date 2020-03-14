b5fa9f66a00aafc1feebb8676bc0e09cc742e9ce
tom1484 <angelus0918866416@gmail.com>
2020-03-14 23:22:21 +0800

    update

---
+++ include.js
@@ -0,0 +1,12 @@
+// (function() {
+function include(post_name) {
+	var xhr= new XMLHttpRequest();
+	xhr.open('GET', 'posts/'+post_name+'/preview.html', true);
+	xhr.onreadystatechange= function() {
+		if (this.readyState !== 4) return;
+		if (this.status !== 200) return; // or whatever error handling you want
+		document.getElementById('y').innerHTML= this.responseText;
+	};
+	xhr.send();
+}
+// })();
