fa0913efea96024e686fdee30b24c1444d92692b
tom1484 <angelus0918866416@gmail.com>
2020-03-14 23:29:04 +0800

    update

--- include.js
+++ include.js
@@ -5,7 +5,7 @@ function include(post_name) {
 	xhr.onreadystatechange= function() {
 		if (this.readyState !== 4) return;
 		if (this.status !== 200) return; // or whatever error handling you want
-		document.getElementById('y').innerHTML= this.responseText;
+		document.getElementById(post_name).innerHTML= this.responseText;
 	};
 	xhr.send();
 }
