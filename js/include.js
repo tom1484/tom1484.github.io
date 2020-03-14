a8ded19e3a18c7a7e9f1e74a7a60b1e3734133b0
tom1484 <angelus0918866416@gmail.com>
2020-03-15 01:18:08 +0800

    update

--- js/include.js
+++ js/include.js
@@ -1,5 +1,4 @@
-// (function() {
-function include(post_name) {
+function includePreview(post_name) {
 	var xhr= new XMLHttpRequest();
 	xhr.open('GET', 'posts/'+post_name+'/preview.html', true);
 	xhr.onreadystatechange= function() {
@@ -9,4 +8,14 @@ function include(post_name) {
 	};
 	xhr.send();
 }
-// })();
+
+function includeDetail(post_name) {
+	var xhr= new XMLHttpRequest();
+	xhr.open('GET', 'posts/'+post_name+'/detail.html', true);
+	xhr.onreadystatechange= function() {
+		if (this.readyState !== 4) return;
+		if (this.status !== 200) return; // or whatever error handling you want
+		document.getElementById(post_name).innerHTML= this.responseText;
+	};
+	xhr.send();
+}
