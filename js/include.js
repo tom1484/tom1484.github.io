e86cff7a96a9d427204284442b942ebc0dc26036
tom1484 <angelus0918866416@gmail.com>
2020-03-21 12:07:48 +0800

    update

--- js/include.js
+++ js/include.js
@@ -1,13 +1,21 @@
 function includePreview(category) {
 
-	list = document.getElementById("list");
+	var list = document.getElementById("list");
+	var posts;
+	var posts_meta = [];
 
 	var xhr= new XMLHttpRequest();
-	xhr.open('GET', 'posts/posts.json', true);
+	xhr.open('GET', 'posts/posts.json', false);
 	xhr.onreadystatechange= function() {
 		if (this.readyState !== 4) return;
 		if (this.status !== 200) return; // or whatever error handling you want
-		alert(this.responseText);
+		posts = JSON.parse(this.responseText);
 	};
 	xhr.send();
+
+	for (var post in posts) {
+		posts_meta.push(posts[post]);
+	}
+	alert(posts_meta);
+	
 }
