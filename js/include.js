ea684f085a256e1e338424adc572dfe35dbceb85
tom1484 <angelus0918866416@gmail.com>
2020-03-21 10:56:52 +0800

    update

--- js/include.js
+++ js/include.js
@@ -1,21 +1,33 @@
-function includePreview(post_name) {
-	var xhr= new XMLHttpRequest();
-	xhr.open('GET', 'posts/'+post_name+'/preview.html', true);
-	xhr.onreadystatechange= function() {
-		if (this.readyState !== 4) return;
-		if (this.status !== 200) return; // or whatever error handling you want
-		document.getElementById(post_name).innerHTML= this.responseText;
-	};
-	xhr.send();
+function includePreview(category) {
+
+	list = document.getElementById("list");
+	posts = readDirectory("posts");
+
+	// var xhr= new XMLHttpRequest();
+	// xhr.open('GET', 'posts/'+post_name+'/preview.html', true);
+	// xhr.onreadystatechange= function() {
+		// if (this.readyState !== 4) return;
+		// if (this.status !== 200) return; // or whatever error handling you want
+		// document.getElementById(post_name).innerHTML= this.responseText;
+	// };
+	// xhr.send();
 }
 
-function includeDetail(post_name) {
-	var xhr= new XMLHttpRequest();
-	xhr.open('GET', 'posts/'+post_name+'/detail.html', true);
-	xhr.onreadystatechange= function() {
-		if (this.readyState !== 4) return;
-		if (this.status !== 200) return; // or whatever error handling you want
-		document.getElementById(post_name).innerHTML= this.responseText;
-	};
-	xhr.send();
-}
+function readDirectory(dir) {
+
+	var filesystem = require("fs");
+	var results = [];
+
+	filesystem.readdirSync(dir).forEach(function(file) {
+
+		file = dir+'/'+file;
+		var stat = filesystem.statSync(file);
+
+		if (stat && stat.isDirectory()) {
+			results = results.concat(_getAllFilesFromFolder(file))
+		} else results.push(file);
+
+	});
+
+	return results;
+};
