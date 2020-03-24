97dfcbb93adeb72e0635b6366975f6f9f0d75d57
tom1484 <angelus0918866416@gmail.com>
2020-03-24 17:38:59 +0800

    update

--- js/include.js
+++ js/include.js
@@ -1,3 +1,79 @@
+// function includePreview(category) {
+
+// 	var posts;
+// 	var meta = [];
+
+// 	var xhr= new XMLHttpRequest();
+// 	xhr.open('GET', 'posts/posts.json', false);
+// 	xhr.onreadystatechange= function() {
+
+// 		if (this.readyState !== 4) 
+// 			return;
+// 		if (this.status !== 200) 
+// 			return; // or whatever error handling you want
+// 		posts = JSON.parse(this.responseText);
+	
+// 	};
+// 	xhr.send();
+
+// 	for (var i in posts) {
+	
+// 		var xhr= new XMLHttpRequest();
+// 		xhr.open('GET', 'posts/'+posts[i]+'/meta-data.json', true);
+// 		xhr.onreadystatechange= function() {
+	
+// 			if (this.readyState !== 4) 
+// 				return;
+// 			if (this.status !== 200) 
+// 				return; // or whatever error handling you want
+// 			meta.push(JSON.parse(this.responseText));
+
+// 			if (i == posts.length-1) {
+				
+// 				function compareTime(a, b) {
+// 					if (a["time"] < b["time"])
+// 						return -1;
+// 					if (a["time"] > b["time"])
+// 						return 1;
+// 					return 0;
+// 				}
+// 				meta.sort(compareTime);
+
+// 				for (var j in meta) {
+		
+// 					var folder = meta[j]["folder"];
+// 					var xhr = new XMLHttpRequest();
+// 					xhr.open('GET', 'posts/'+folder+'/preview.html', true);
+// 					xhr.onreadystatechange = function() {
+				
+// 						if (this.readyState !== 4) 
+// 							return;
+// 						if (this.status !== 200) 
+// 							return; // or whatever error handling you want
+// 						meta[j]["preview"] = this.responseText;
+						
+// 						if (j == meta.length-1) {
+// 							list = document.getElementById("list");
+// 							for (var k in meta)
+// 								list.innerHTML += meta[k]["preview"]			
+// 						}
+// 					};
+// 					xhr.send();
+	
+// 	}
+
+// 			}
+		
+// 		};
+// 		xhr.send();	
+	
+// 	}
+
+	
+
+// }
+
+
 function includePreview(category) {
 
 	var posts;
@@ -19,8 +95,8 @@ function includePreview(category) {
 	for (var i in posts) {
 	
 		var xhr= new XMLHttpRequest();
-		xhr.open('GET', 'posts/'+posts[i]+'/meta-data.json', true);
-		xhr.onreadystatechange= function() {
+		xhr.open('GET', 'posts/'+posts[i]+'/meta-data.json', false);
+		xhr.onreadystatechange = function() {
 	
 			if (this.readyState !== 4) 
 				return;
@@ -28,47 +104,41 @@ function includePreview(category) {
 				return; // or whatever error handling you want
 			meta.push(JSON.parse(this.responseText));
 
-			if (i == posts.length-1) {
-				
-				function compareTime(a, b) {
-					if (a["time"] < b["time"])
-						return -1;
-					if (a["time"] > b["time"])
-						return 1;
-					return 0;
-				}
-				meta.sort(compareTime);
-
-				for (var j in meta) {
-		
-					var folder = meta[j]["folder"];
-					var xhr = new XMLHttpRequest();
-					xhr.open('GET', 'posts/'+folder+'/preview.html', true);
-					xhr.onreadystatechange = function() {
-				
-						if (this.readyState !== 4) 
-							return;
-						if (this.status !== 200) 
-							return; // or whatever error handling you want
-						meta[j]["preview"] = this.responseText;
-						
-						if (j == meta.length-1) {
-							list = document.getElementById("list");
-							for (var k in meta)
-								list.innerHTML += meta[k]["preview"]			
-						}
-					};
-					xhr.send();
-	
-	}
-
-			}
-		
 		};
 		xhr.send();	
 	
 	}
 
+	for (var i in meta) {
+
+		var folder = meta[i]["folder"];
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'posts/'+folder+'/preview.html', false);
+		xhr.onreadystatechange = function() {
 	
+			if (this.readyState !== 4) 
+				return;
+			if (this.status !== 200) 
+				return; // or whatever error handling you want
+			meta[i]["preview"] = this.responseText;
+			
+		};
+		xhr.send();
+	
+	}
+
+	function compareTime(a, b) {
+		if (a["time"] < b["time"])
+			return -1;
+		if (a["time"] > b["time"])
+			return 1;
+		return 0;
+	}
+	meta.sort(compareTime);
+
+
+	list = document.getElementById("list");
+	for (var i in meta)
+		list.innerHTML += meta[i]["preview"]			
 
 }
